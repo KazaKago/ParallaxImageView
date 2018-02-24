d22d321fb92c8225669a6c6c0482d34b34ddef21
KazaKago <weathercock_perch@yahoo.co.jp>
2018-02-25 02:22:55 +0900

    first commit.

---
+++ library/src/main/java/com/kazakago/parallaximageview/ParallaxImageView.kt
@@ -0,0 +1,87 @@
+package com.kazakago.parallaximageview
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.Icon
+import android.net.Uri
+import android.os.Build
+import android.support.annotation.DrawableRes
+import android.support.annotation.RequiresApi
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import android.widget.ImageView
+
+open class ParallaxImageView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : FrameLayout(context, attrs, defStyleAttr) {
+
+    enum class Direction(val value: Int) {
+        Forward(1),
+        Reverse(-1),
+    }
+
+    companion object {
+        const val defaultDistance = 100f
+    }
+
+    open var direction: Direction = Direction.Forward
+    open var distance: Float = defaultDistance
+
+    val nativeImageView: ImageView
+
+    init {
+        val rootView = LayoutInflater.from(context).inflate(R.layout.view_parallax_image, this)
+        nativeImageView = rootView.findViewById(R.id.nativeImageView)
+
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ParallaxImageView, defStyleAttr, 0)
+        nativeImageView.setImageResource(typedArray.getResourceId(R.styleable.ParallaxImageView_src, 0))
+        typedArray.recycle()
+
+        viewTreeObserver.addOnDrawListener {
+            dispatchParallax()
+        }
+    }
+
+    private fun dispatchParallax() {
+        val getLocationInWindowPos = IntArray(2)
+        getLocationInWindow(getLocationInWindowPos)
+
+        val getWindowVisibleDisplayFrameRect = Rect()
+        getWindowVisibleDisplayFrame(getWindowVisibleDisplayFrameRect)
+
+        val visiblePercent = getLocationInWindowPos[1].toFloat() / (getWindowVisibleDisplayFrameRect.bottom - getWindowVisibleDisplayFrameRect.top).toFloat()
+        val translationY = distance * (visiblePercent - 0.5f) * direction.value
+        nativeImageView.translationY = translationY
+    }
+
+    public fun setImageBitmap(bitmap: Bitmap?) {
+        nativeImageView.setImageBitmap(bitmap)
+    }
+
+    public fun setImageDrawable(drawable: Drawable?) {
+        nativeImageView.setImageDrawable(drawable)
+    }
+
+    public fun setImageResource(@DrawableRes resId: Int) {
+        nativeImageView.setImageResource(resId)
+    }
+
+    public fun setImageURI(uri: Uri?) {
+        nativeImageView.setImageURI(uri)
+    }
+
+    public fun setImageLevel(level: Int) {
+        nativeImageView.setImageLevel(level)
+    }
+
+    public fun setImageState(state: IntArray?, merge: Boolean) {
+        nativeImageView.setImageState(state, merge)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    public fun setImageIcon(icon: Icon?) {
+        nativeImageView.setImageIcon(icon)
+    }
+
+}
---
+++ sample/src/main/java/com/kazakago/parallaximageview/sample/MainRecyclerAdapter.kt
@@ -0,0 +1,36 @@
+package com.kazakago.parallaximageview.sample
+
+import android.content.Context
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.view_cell.view.*
+
+class MainRecyclerAdapter(private val context: Context) : RecyclerView.Adapter<MainRecyclerAdapter.ViewHolder>() {
+
+    override fun getItemCount(): Int {
+        return 10
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainRecyclerAdapter.ViewHolder {
+        return ViewHolder(context, parent)
+    }
+
+    override fun onBindViewHolder(holder: MainRecyclerAdapter.ViewHolder, position: Int) {
+        holder.itemView.setBackgroundColor(ContextCompat.getColor(context, when (position % 2) {
+            0 -> R.color.cell1
+            else -> R.color.cell2
+        }))
+        holder.itemView.parallaxImageView.setImageResource(when (position % 5) {
+            0 -> R.drawable.img_q1
+            1 -> R.drawable.img_q2
+            2 -> R.drawable.img_q3
+            3 -> R.drawable.img_q4
+            else -> R.drawable.img_q5
+        })
+    }
+
+    inner class ViewHolder(context: Context, parent: ViewGroup) : RecyclerView.ViewHolder(LayoutInflater.from(context).inflate(R.layout.view_cell, parent, false))
+
+}
